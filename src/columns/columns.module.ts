ff776f11559c8bdc8f45528781717a5ae2020680
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-09-07 18:49:50 -0300

    feat(columns): update columns module

--- src/columns/columns.module.ts
+++ src/columns/columns.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ColumnsController } from './columns.controller';
 import { ColumnsService } from './columns.service';
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
+  imports: [AuthModule],
   controllers: [ColumnsController],
   providers: [ColumnsService],
   exports: [ColumnsService],
