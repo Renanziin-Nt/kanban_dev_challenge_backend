119c303bd984c431795655eae28db233d48a7bb0
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-09-07 18:51:17 -0300

    feat(uploads): add uploads module

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UploadsController } from './uploads.controller';
 import { UploadsService } from './uploads.service';
-
+import { AuthModule } from '../auth/auth.module';
 @Module({
+  imports: [AuthModule],
   controllers: [UploadsController],
   providers: [UploadsService],
   exports: [UploadsService],
