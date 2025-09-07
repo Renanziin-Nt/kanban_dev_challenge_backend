2c4ef255de9e2a1331285ceb3f8b0f4d0add4c97
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-09-07 18:51:04 -0300

    refactor(prisma): update prisma service and module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,6 +3,15 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      transactionOptions: {
+        maxWait: 5000,  // Tempo máximo para aguardar uma transação iniciar
+        timeout: 10000, // Timeout padrão das transações (10 segundos)
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
     console.log('✅ Database connected successfully');
@@ -15,9 +24,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
 
   async cleanDatabase() {
     if (process.env.NODE_ENV === 'production') return;
-    
     const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
-
     return Promise.all(
       models.map((modelKey) => (this as any)[modelKey].deleteMany()),
     );
